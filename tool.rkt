24c73dab0e9525491c7fca23f8ebf5c737fef692
David Christiansen <david@davidchristiansen.dk>
2016-12-12 17:11:08 -0500

    Support multiple tabs in graphical goal viewer

--- tool.rkt
+++ tool.rkt
@@ -3,8 +3,6 @@
 
 (provide tool@)
 
-
-
 (define-runtime-path expansion-handler.rkt "expansion-handler.rkt")
 
 (define tool@
@@ -20,69 +18,89 @@
       (when f
         (f msg)))
 
-    (define goal-setter (box #f))
-    (define (set-goal-list gs)
-      (define f (unbox goal-setter))
-      (when f
-        (f gs)))
-
-    (define editor-mover (box #f))
-    (define (editor-move-to pos [end 'same])
-      (define f (unbox editor-mover))
-      (when f
-        (f pos end)))
-
-    (define hole-info #f)
-    (define (update-hole-info! [info #f])
-      (set! hole-info (and info (make-interval-map info)))
-      (if hole-info
-          (set-goal-list (for/list ([(k v) (in-dict hole-info)])
-                           (list k v)))
-          (set-goal-list null)))
 
     (define hole-finding-text-mixin
-      (mixin (racket:text<%>) ()
+      (mixin (racket:text<%> drracket:unit:definitions-text<%>) ()
         (super-new)
 
-        (inherit get-start-position set-position get-active-canvas scroll-to-position)
+        (inherit get-start-position set-position get-active-canvas scroll-to-position get-tab)
 
         (define (update-pos)
           (define pos (get-start-position))
+          (define tab-info (hash-ref hole-info (get-tab) #f))
           (define h (and hole-info
-                         (interval-map-ref hole-info pos #f)))
+                         tab-info
+                         (interval-map-ref tab-info pos #f)))
           (if h
               (echo (format "Pos: ~a, hole: ~a" pos h))
               (echo (format "Pos: ~a" pos))))
 
-        (define/public (set-goals gs)
-          (update-hole-info! gs)
+        (define hole-info (make-hasheq))
+        (define (set-hole-info! [info #f])
+          (define tab (get-tab))
+          (hash-set! hole-info tab
+                     (and info (make-interval-map info)))
+          (update-hole-info!))
 
-          (echo (format "Goals: ~a" gs)))
+        (define/public (update-hole-info!)
+          (define tab (get-tab))
+          (define frame (send tab get-frame))
+          (match (hash-ref hole-info tab #f)
+            [#f (send frame set-goal-list
+                    null)]
+            [info
+             (send frame set-goal-list
+                   (for/list ([(k v) (in-dict info)])
+                     (list k v)))]))
+
+        (define/public (set-goals gs)
+          (set-hole-info! gs))
 
         (define/augment (after-set-position)
-          (update-pos))
-
-        (set-box! editor-mover
-                  (lambda (pos [end 'same])
-                    (queue-callback (thunk (set-position pos end)))
-                    (queue-callback (thunk (scroll-to-position pos #f end)))
-                    (define canvas (get-active-canvas))
-                    (when canvas
-                      (send canvas focus))))))
+          (update-pos))))
 
     (define extra-panel-mixin
       (mixin (drracket:unit:frame<%>) ()
-        (super-new)
+
+        (inherit get-definitions-text)
+
+        (define/augment (on-tab-change from to)
+          (send (get-definitions-text) update-hole-info!))
+
+        (define goal-list-listeners (box null))
+        (define/public (set-goal-list gs)
+          (for ([l (unbox goal-list-listeners)])
+            (send l on-new-goal-list gs)))
 
         (define/override (get-definitions/interactions-panel-parent)
           (define super-res (super get-definitions/interactions-panel-parent))
           (define new-panel
-            (new panel:vertical-dragable% [parent super-res]))
-          (define p
+            (new (class panel:vertical-dragable%
+                   (super-new)
+                   (inherit change-children)
+                   (define/public (on-new-goal-list gs)
+                     (change-children
+                      (lambda (subareas)
+                        (append
+                         (for/list ([area (in-list subareas)]
+                                    #:when (not (eq? area hole-holder)))
+                           area)
+                         (if (null? gs)
+                             null
+                             (list hole-holder)))))))
+                 [parent super-res]))
+          (define hole-holder
             (new panel:horizontal-dragable% [parent new-panel] [style '(deleted)]))
-          (define msg
-            (new list-box%
-                 [parent p]
+          (define hole-list-box
+            (new (class list-box%
+                   (super-new)
+                   (inherit append clear)
+                   (define/public (on-new-goal-list gs)
+                     (clear)
+                     (for ([g gs])
+                       (send hole-list-box append
+                             (format "~a" g) (goal-info (caar g) (cdar g) (cdr g))))))
+                 [parent hole-holder]
                  [label #f]
                  [choices (list "Hole 1" "Hole 2" "...")]
                  [style '(single)]
@@ -94,11 +112,17 @@
                                  [(list n)
                                   (match-define (goal-info start end meta)
                                     (send list-box get-data n))
-                                  (editor-move-to start end)]
+                                  (define defns (get-definitions-text))
+                                  (queue-callback (thunk (send* defns
+                                                           (set-position start end)
+                                                           (scroll-to-position start #f end))
+                                                         (define canvas (send defns get-active-canvas))
+                                                         (when canvas (send canvas focus))))]
                                  [_ (void)])))]))
+
           (define p2
             (new group-box-panel%
-                 [parent p]
+                 [parent hole-holder]
                  [label "Details"]))
           (define echo-area
             (new text-field%
@@ -106,29 +130,14 @@
                  [label #f]
                  [style '(multiple)]))
 
+          (set-box! goal-list-listeners (list hole-list-box new-panel))
+
           (set-box! echoer
                     (lambda (new-msg)
                       (queue-callback (thunk (send echo-area set-value new-msg)))))
+          new-panel)
 
-          (set-box! goal-setter
-                    (lambda (gs)
-                      (send msg clear)
-                      (if (null? gs)
-                          (send new-panel change-children (lambda (subareas)
-                                                            (for/list ([area (in-list subareas)]
-                                                                       #:when (not (eq? area p)))
-                                                              area)))
-                          (begin
-                            (send new-panel change-children
-                                  (lambda (subareas)
-                                    (append
-                                     (for/list ([area (in-list subareas)]
-                                                #:when (not (eq? area p)))
-                                       area)
-                                     (list p))))
-                            (for ([g gs])
-                              (send msg append (format "~a" g) (goal-info (caar g) (cdar g) (cdr g))))))))
-          new-panel)))
+        (super-new)))
 
     (define (phase1) (void))
     (define (phase2) (void))
